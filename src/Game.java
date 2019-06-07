a3d9216416fc70553a303cc5f7a7346f09d04be6
Mouse4TheTurtle <dillongeils@gmail.com>
2019-06-07 01:09:05 -0700

    3rd Commit
    Changes
    
    - Added a way to play a sad version of chess using the console.

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Game
 {
@@ -25,8 +26,39 @@ public class Game
         */
 
         //gamer.movePiece(gamer.getBoard()[7][1],new Movement(2,1));
-        gamer.movePiece(gamer.getBoard()[6][5],new Movement(-1,0));
-        System.out.println(gamer);
+        //gamer.movePiece(gamer.getBoard()[6][5],new Movement(-1,0));
+        //System.out.println(gamer);
+        String response = "";
+        int x;
+        int y;
+        int w;
+        int z;
+        while (!response.toLowerCase().equals("exit"))
+        {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println(gamer);
+            System.out.println("\nMove which piece? ");
+            System.out.println("Row? ");
+            x = keyboard.nextInt();
+            System.out.println("Column? ");
+            y = keyboard.nextInt();
+            System.out.println("Moving " + gamer.getBoard()[x][y].getColor() + gamer.getBoard()[x][y].getPieceName());
+            System.out.println("Move on the vertical axis by? ");
+            w = keyboard.nextInt();
+            System.out.println("Move on the horizontal axis by? ");
+            z = keyboard.nextInt();
 
+            gamer.movePiece(gamer.getBoard()[x][y], new Movement(w,z));
+
+            System.out.println(gamer);
+
+            System.out.println("Restart, exit, or continue?");
+            response = keyboard.nextLine();
+            if(response.toLowerCase().equals("restart"))
+            {
+                gamer.resetBoard();
+            }
+
+        }
     }
 }
