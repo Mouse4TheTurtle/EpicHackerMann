9fd464207d5cb811d3ed130bbb12fc4188df48a9
popeAnthrax <Theeventhoizon@gmail.com>
2019-06-08 21:22:50 -0700

    GUI edit

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,4 @@
-package javatutorial.net;
+//package javatutorial.net;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -6,15 +6,15 @@ import javax.swing.JPanel;
 public class GUI extends JPanel{
 private int x = 10;
     public void paint(Graphics g){
-    //    for(;x <= 138; x =+16) {
-            g.drawRect( x, 10, 16, 16);
-      //  }
+        for(;x <= 138; x =+16) {
+            g.drawRect( 10, 10, 16, 16);
+        }
     }
 
     public static void main(String[] args){
         JFrame frame= new JFrame("Chess Game");
-        frame.getContentPane().add(new javatutorial.net.GUI());
-        frame.setSize(300, 300);
+  //      frame.getContentPane().add(new javatutorial.net.GUI());
+        frame.setSize(1000, 1000);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
