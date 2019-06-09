2784551685c318f7a841261b0d362217100814cc
popeAnthrax <Theeventhoizon@gmail.com>
2019-06-08 21:46:36 -0700

    real GUI edit

--- src/GUI.java
+++ src/GUI.java
@@ -1,22 +1,5 @@
-//package javatutorial.net;
-import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-public class GUI extends JPanel{
-private int x = 10;
-    public void paint(Graphics g){
-        for(;x <= 138; x =+16) {
-            g.drawRect( 10, 10, 16, 16);
-        }
-    }
-
-    public static void main(String[] args){
-        JFrame frame= new JFrame("Chess Game");
-  //      frame.getContentPane().add(new javatutorial.net.GUI());
-        frame.setSize(1000, 1000);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-    }
-}	
+public class GUI {
+    private JPanel panel1;
+}
