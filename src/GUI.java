1af99ea0c477ccfef9fe3e08cda9911291185285
Mouse4TheTurtle <dillongeils@gmail.com>
2019-06-09 11:00:08 -0700

    Merge branch 'master' of D:\Users\dilly\IdeaProjects\EpicHackerMann with conflicts.

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,22 @@
-import javax.swing.*;
+//package javatutorial.net;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class GUI {
-    private JPanel panel1;
-}
+public class GUI extends JPanel{
+private int x = 10;
+    public void paint(Graphics g){
+        for(;x <= 138; x =+16) {
+            g.drawRect( 10, 10, 16, 16);
+        }
+    }
+
+    public static void main(String[] args){
+        JFrame frame= new JFrame("Chess Game");
+  //    frame.getContentPane().add(new javatutorial.net.GUI());
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+}	
