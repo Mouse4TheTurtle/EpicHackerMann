93490f323ce7dec50117fb80e06d584ca4d8b405
popeAnthrax <Theeventhoizon@gmail.com>
2019-07-18 23:33:00 -0700

    Bish val edit

--- src/Engine.java
+++ src/Engine.java
@@ -1,12 +1,29 @@
 public class Engine {
     private static Board game = new Board();
-    public static void main(String[] args) {
+    private Piece[][] gameBoard1;
+    private Piece[][] gameBoard2;
+    private Piece[][] gameBoard3;
+    private Piece[][] gameBoard4;
+    private Piece[][] gameBoard5;
+    private Piece[][] gameBoard6;
+    private Piece[][] gameBoard7;
+    private Piece[][] gameBoard8;
+
+    public void main(String[] args) {
+        game.getGameBoard();
 
         double whiteValue = 0;
         double blackValue = 0;
 
-        game.getGameBoard();
-        game.movePiece(" c3");
+    gameBoard1 = new Piece[8][8];
+    gameBoard2 = new Piece[8][8];
+    gameBoard3 = new Piece[8][8];
+    gameBoard4 = new Piece[8][8];
+    gameBoard5 = new Piece[8][8];
+    gameBoard6 = new Piece[8][8];
+    gameBoard7 = new Piece[8][8];
+    gameBoard8 = new Piece[8][8];
 
+    game.getGameBoard()= gameBoard1;
     }
 }
